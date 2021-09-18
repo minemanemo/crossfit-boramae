fb50b1e9108260efe53135e5ca3187ba2f500338
이민회(Minhoe Lee)/Payment데이터팀/11ST <minhoe.lee@sk.com>
2021-09-18 18:22:44 +0900

    Implement API

--- pages/api/attendance.ts
+++ pages/api/attendance.ts
@@ -1,9 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import chromium from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer';
 import type { Page } from 'puppeteer';
 import { decode } from 'html-entities';
+
+function getUrl(y: number, m: number, d: number, page?: number): string {
+  const base = 'https://cafe.naver.com/AttendanceView.nhn';
+  const clubid = '27959802';
+  const menuid = '28';
+  const urlWithDatetime = `${base}?search.clubid=${clubid}&search.menuid=${menuid}&search.attendyear=${y}&search.attendmonth=${m}&search.attendday=${d}`;
+
+  if (page === undefined) {
+    urlWithDatetime;
+  }
+
+  return `${urlWithDatetime}&search.page=${page}`;
+}
 
 async function login(page: Page) {
   const naver_id = 'borame-crossfit-bot';
@@ -25,30 +36,101 @@ async function login(page: Page) {
   });
 }
 
-async function getAttendanceBook(page: Page): Promise<string[]> {
-  const url = `https://cafe.naver.com/AttendanceView.nhn?search.clubid=27959802&search.menuid=28&search.attendyear=2021&search.attendmonth=9&search.attendday=15`;
-
+async function getAttendanceBook(
+  page: Page,
+  y: number,
+  m: number,
+  d: number
+): Promise<string[]> {
+  const url = getUrl(y, m, d);
   await page.goto(url);
 
   const iframeSelector = '#main-area iframe';
   const elementHandle = await page.$(iframeSelector);
   const frame = await elementHandle?.contentFrame();
+  const totalPageSelector = '.attendance_lst_section .prev-next a';
+  const totalPage = await frame?.$$eval(totalPageSelector, (els) => els.length);
 
+  let result: string[] = [];
   const attendanceSelector = '.list_attendance li .cmt .txt';
-  const data = await frame?.$$eval(attendanceSelector, (els) =>
-    els.map((el) => el.innerHTML)
-  );
+  for (let i = 1; i <= (totalPage || 0); i++) {
+    await page.goto(getUrl(y, m, d, i));
 
-  const result = data
-    ?.map((d) => decode(d))
-    .filter((d) => d.indexOf('수업현황') < 0);
+    const elementHandle = await page.$(iframeSelector);
+    const frame = await elementHandle?.contentFrame();
+    const data = await frame?.$$eval(attendanceSelector, (els) =>
+      els.map((el) => el.innerHTML)
+    );
+    result = [...result, ...(data || [])];
+  }
 
-  return result || [];
+  return result?.map((d) => decode(d.replace(/\s/g, ' ')));
 }
 
 type Data = {
-  result: string[];
+  data: Reserve[];
 };
+
+export type Reserve = {
+  time: string;
+  name: string;
+  num: string;
+  comment: string;
+  cancel?: boolean;
+};
+
+const splitStrToObj = (str: string): Reserve => {
+  const sp = str.split(' ');
+  const [time, name, num, ...comment] = sp.filter((s) => s !== '');
+  return {
+    time,
+    name,
+    num: num || '????',
+    comment: comment.join(' '),
+    cancel: false,
+  };
+};
+
+function parseResult(data: string[]): Reserve[] {
+  const attend: string[] = [];
+  const cancel: string[] = [];
+
+  data.forEach((d) => {
+    const str = d
+      .replace(/<br>/g, '')
+      .replace(/\n/g, ' ')
+      .replace(/^([0-9][0-9])시([0-5][0-9])분(.*)/, '$1:$2 $3')
+      .replace(/^([0-9])시([0-5][0-9])분(.*)/, '$1:$2 $3')
+      .replace(/^([0-9])시(.*)/, '$1:$2 $3')
+      .replace(/^([0-9][0-9])([0-9][0-9])(.*)/, '$1:$2 $3')
+      .replace(/([ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{3})([0-9]{4})/g, '$1 $2')
+      .replace(/\s/g, ' ')
+      .replace(/[ ]+/, ' ');
+
+    if (/^([1-9]|[01][0-9]|2[0-3]):([0-5][0-9])/.test(str)) {
+      if (str.indexOf('취소') < 0) {
+        attend.push(str);
+      } else {
+        cancel.push(str);
+      }
+    }
+  });
+
+  const _attend = attend.sort().map((d) => splitStrToObj(d));
+  const _cancel = cancel.sort().map((d) => splitStrToObj(d));
+
+  _cancel.forEach((c) => {
+    const findIndex = _attend.findIndex(
+      (a) => a.time === c.time && a.name === c.name && a.num === c.num
+    );
+    if (findIndex >= 0) {
+      const newComment = `${_attend[findIndex].comment} / ${c.comment}`;
+      _attend[findIndex].comment = newComment;
+      _attend[findIndex].cancel = true;
+    }
+  });
+  return _attend;
+}
 
 export default function handler(
   req: NextApiRequest,
@@ -62,14 +144,18 @@ export default function handler(
       headless: true,
       ignoreHTTPSErrors: true,
     });
+
+    const y = Number(req.query.y);
+    const m = Number(req.query.m);
+    const d = Number(req.query.d);
+
     const page = await browser.newPage();
-
     await login(page);
-
-    const result = await getAttendanceBook(page);
-    res.status(200).json({ result: result });
-
+    const result = await getAttendanceBook(page, y, m, d);
     await browser.close();
+
+    const parsed = parseResult(result);
+    res.status(200).json({ data: parsed });
   };
 
   crawler();
